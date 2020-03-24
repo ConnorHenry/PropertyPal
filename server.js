629c0336b05277994f46a225d7b5045eb94af3f2
ConnorHenry <conorhenry@yahoo.co.uk>
2020-03-24 10:57:46 +0000

    make it better

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
   console.log("This is working")
 });
 
