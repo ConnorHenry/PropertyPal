cb31bef4a1048cd508b4dfd871e8c5c720e97c37
ConnorHenry <conorhenry@yahoo.co.uk>
2020-03-24 10:50:19 +0000

    make it better

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/', function (req, res) {
   console.log("This is working")
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
